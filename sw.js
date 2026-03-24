0dc747eb8ea4cffcd5efcad177668ebc13ad944f
nbrfr55557-jpg <pierrick.stm@gmail.com>
2026-03-24 19:52:34 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '2.0.6';
+const VERSION = '2.0.7';
 const CACHE = 'bar-vampires-' + VERSION;
 const FILES = ['./', './index.html', './manifest.json', './icon-512.png'];
 
@@ -13,12 +13,6 @@ self.addEventListener('activate', e => {
     caches.keys()
       .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
-      .then(() => {
-        // Forcer tous les onglets ouverts à recharger avec la nouvelle version
-        return self.clients.matchAll({type:'window'}).then(clients => {
-          clients.forEach(client => client.navigate(client.url));
-        });
-      })
   );
 });
 
